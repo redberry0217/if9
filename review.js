63b05fe26a085f9c5884e53c3d38722970aa8bb9
SohyangO <shyang903@naver.com>
2024-01-15 15:33:11 +0900

    리뷰 빈 칸일 때 alert 창 추가

--- review.js
+++ review.js
@@ -3,6 +3,7 @@ const nickname = document.getElementById("nickname");
 const password = document.getElementById("password");
 const review = document.getElementById("review");
 const writeBtn = document.getElementById("writeBtn");
+const nothing = document.getElementById("nothing");
 // 현재 시간
 const today = new Date();
 const year = today.getFullYear();
@@ -24,6 +25,7 @@ const getComments = () => {
 
   if (comments.length >= 1) {
     //Cannot read properties of null (reading 'length')
+    nothing.remove();
     let revealArr = JSON.parse(comments);
     revealArr.forEach((e) => {
       let stackReview = document.getElementById("stackReview");
@@ -48,7 +50,28 @@ function mkReview() {
     password: password.value,
     review: review.value
   };
-  const arr = [];
+
+  //빈칸인 채로 입력 시, 경고문구 띄우기
+  if (reviewObj.nickname == "") {
+    alert("닉네임을 입력해주세요.");
+    nickname.focus();
+    return;
+  } else if (reviewObj.password == "") {
+    alert("비밀번호를 입력해주세요.");
+    password.focus();
+    return;
+  } else if (reviewObj.review == "") {
+    alert("리뷰를 작성해주세요.");
+    review.focus();
+    return;
+  }
+
+  if (reviewObj.password.length < 4 || reviewObj.password.length > 6) {
+    alert("비밀번호를 4~6글자 사이로 설정해주세요.");
+    password.focus();
+    return;
+  }
+  const arr = []; //이전에 작성했던 데이터 초기화 및 새로 작성한 데이터 저장
 
   if (JSON.parse(localStorage.getItem(`comments${id}`)) !== null) {
     arr.push(...JSON.parse(localStorage.getItem(`comments${id}`)));
@@ -60,20 +83,24 @@ function mkReview() {
   }
 
   let writeComment = localStorage.getItem(`comments${id}`);
+
   let tempArr = JSON.parse(writeComment);
+
+  console.log(writeComment.nickname);
   let [commentBox] = document.getElementsByClassName("commentBox");
   const addComment = document.createElement("ul");
-  console.log(addComment);
+  console.log(commentBox);
   tempArr.forEach((e) => {
     addComment.innerHTML = `
       <li>${e.nickname}</li>
+      <span>${formatDate}</span>
       <hr></hr>
       <li> ${e.review}</li>`;
   });
-  console.log(commentBox);
   commentBox.prepend(addComment);
 }
-document.getElementById("review").addEventListener("keydown", ({ key }) => {
+
+reviewDiv.addEventListener("keydown", ({ key }) => {
   if (key !== "Enter") {
     return;
   }
